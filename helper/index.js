0496b83e23b5cc0b83f96cab6527102f295d7a31
Johann Exconde <johann.exconde@gmail.com>
2020-09-21 21:42:55 +0800

    added cors policy to communicate with fe

--- helper/index.js
+++ helper/index.js
@@ -2,10 +2,12 @@ const errorLogger = require('./errorLogger');
 const errorHandler = require('./errorHandler');
 const requestValidator = require('./requestValidator');
 const dateValidator = require('./dateValidator');
+const setHeaders = require('./setHeaders');
 
 module.exports = {
     errorLogger,
     errorHandler,
     requestValidator,
-    dateValidator
+    dateValidator,
+    setHeaders
 };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {
     inMemoryMongo
 } = require('./lib');
 
-const port = 3000;
+const port = 4000;
 
 (async () => {
     await inMemoryMongo.connect();
